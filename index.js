3f4c4c8e9cc48f4e16fe841de9d1278e0ff30a88
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-26 09:03:41 -0300

    It is added express configuration

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hola mundo111')
+const express = require('express');
+
+// Create express server
+
+const app = express();
+const PORT =4000 ;
+
+//
+
+app.get('/',(req,res)=>{
+    res.status(200).json('gola')
+})
+
+
+// listeninig 
+
+
+app.listen(PORT, () => console.log('Server listening on port '+ PORT))
