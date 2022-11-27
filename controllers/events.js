4cc2dcd0807fd69ae833c414009032fc9690d31f
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-27 10:39:00 -0300

    It is added udate event method

--- controllers/events.js
+++ controllers/events.js
@@ -52,15 +52,48 @@ const event = await Event.find().populate('user','name')
     })
   
    }
-   const putEvents = (req,res=response) => {
+   const putEvents = async(req,res=response) => {
 
-    const { id } = req.params;
+    const  eventId  = req.params.id;
+
+    const uid = req.uid;
+
+    try {
+      const event = await Event.findById( eventId );
+      console.log(event)
+      if( !event){
+        return res.status(404).json({
+          status:false,
+          msg:'It does not exist the event with this Id',
+          
+        })
+      }
+      if( event.user.toString() !== uid ){
+       return res.status(401).json({
+          ok:false,
+          msg:'You can not edit this event'
+        })
+      }
+      const newEvent = {
+        ...req.body,
+        user:uid
+      }
+      const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent,{new:true} );
+      return res.status(201).json({
+        ok:true,
+        msg:'Event updated successfully',
+        updatedEvent
+      })
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status:true,
+        msg:'Internal Error',
+        id
+      })
+    }
     
-    return res.status(200).json({
-      status:true,
-      msg:'I am a putEvents',
-      id
-    })
+    
   
    }
 
